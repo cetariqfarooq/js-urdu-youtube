d0edfedb3286856bd77887531782b36d8a5840f8
cetariqfarooq <93148293+cetariqfarooq@users.noreply.github.com>
2024-02-04 19:07:40 +0000

    Constant and Variables

---
+++ .devcontainer/01_Basics/01_Variables.js
@@ -0,0 +1,10 @@
+const accountID = 1214
+let accountEmail = "cetariqfarooq@gmail.com"
+var accountPassword = "121212"
+accoutCity = "Lahore"
+// accountID = 456
+console.log(accountEmail);
+console.log(accountID);
+console.log(accountPassword);
+console.log(accoutCity);
+console.table([accountEmail, accountID, accountPassword, accoutCity])
